85a8addd75b9d038e48b9a87089d2018e537ba9e
ask <tjdrud3632@naver.com>
2021-06-11 15:26:33 +0900

    6/11

---
+++ src/main/java/com/example/demo/controller/CustomErrorController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+
+//@Controller
+//public class CustomErrorController implements ErrorController {
+
+
+//}
