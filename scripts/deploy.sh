4a2581c762003dada44cbedd111d981d7f995ad7
ask <tjdrud3632@naver.com>
2021-06-14 19:09:08 +0900

    6/14 deploy.sh -> jar

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,11 @@ PROJECT_NAME=springboot-webservice
 
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/zip/*.war $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep war | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
@@ -23,17 +23,17 @@ fi
 
 echo "> 새 어플리케이션 배포"
 
-WAR_NAME=$(ls -tr $REPOSITORY/*.war | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> war Name: $WAR_NAME"
+echo "> jar Name: $JAR_NAME"
 
-echo "> $WAR_NAME 에 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod +x $WAR_NAME
+chmod +x $JAR_NAME
 
-echo "> $WAR_NAME 실행"
+echo "> $JAR_NAME 실행"
 
-nohup java -war \
+nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
-  $WAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
