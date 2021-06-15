42be2293c32666e3c6e95406179dc36c494533b2
ask <tjdrud3632@naver.com>
2021-06-15 18:59:02 +0900

    6/15

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @MapperScan(value = "com.example.demo.mapper")
 public class Application {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
