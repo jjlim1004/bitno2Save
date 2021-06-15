13fb99a28cb83a4ffe9de0e7455eb1f18a07e462
ask <tjdrud3632@naver.com>
2021-06-15 20:21:49 +0900

    6/15 1

--- src/main/java/com/example/demo/controller/CustomErrorController.java
+++ src/main/java/com/example/demo/controller/CustomErrorController.java
@@ -1,37 +1,25 @@
 package com.example.demo.controller;
 
-
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.data.relational.core.sql.In;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
+
 
 @Controller
 public class CustomErrorController implements ErrorController {
 
-    private static final String ERROR_PATH = "/error";
 
     @RequestMapping("/error")
-    public String handleError(HttpServletRequest request, Model model) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    public String handleError() {
 
-      //   model.addAttribute("code", status.toString());
-      //  model.addAttribute("msg", httpStatus.getReasonPhrase());
-      //  model.addAttribute("timestamp", new Date());
-
-        return ERROR_PATH+ "/error";
+        return "/error/error";
     }
 
     @Override
     public String getErrorPath() {
-        return ERROR_PATH;
+
+        return "/error";
     }
 
 }
